97383be5ae00c90cb32ace5f3bef315c1e2bf83e
Tafil Kajtazi <tafil.kajtazi@gmail.com>
2018-07-13 22:26:02 +0200

    added debug mode (toggle with F1)

--- code/main.cpp
+++ code/main.cpp
@@ -20,9 +20,9 @@ struct Application_State {
     Memory_Growing_Stack_Allocator_Info transient_memory;
     
     Pixel_Rectangle main_window_area;
+    bool main_window_is_fullscreen;
     
-    Immediate_Render_Context imc;
-    
+    Immediate_Render_Context immediate_render_context;
     UI_Render_Context ui_render_context;
     
     struct UI_Font_Material {
@@ -48,8 +48,11 @@ struct Application_State {
     
     mat4f camera_to_clip_projection;
     
+    struct Camera {
+        union { mat4x3f to_world_transform, inverse_view_matrix; };
+    } debug_camera, camera;
+    
     union { mat4x3f world_to_camera_transform, view_matrix; };
-    union { mat4x3f camera_to_world_transform, inverse_view_matrix; };
     
     f32 debug_camera_alpha;
     f32 debug_camera_beta;
@@ -61,13 +64,15 @@ struct Application_State {
     
     Mesh ship_mesh, astroid_mesh, beam_mesh;
     mat4x3f ship_transform, astroid_transform, beam_transform;
+    
+    bool in_debug_mode;
 };
 
-void update_camera(Application_State *state) {
+void debug_update_camera(Application_State *state) {
     quatf rotation = make_quat(Debug_Camera_Axis_Alpha, state->debug_camera_alpha);
     rotation = multiply(rotation, make_quat(Debug_Camera_Axis_Beta, state->debug_camera_beta));
     
-    state->camera_to_world_transform = make_transform(rotation, state->camera_to_world_transform.translation);
+    state->debug_camera.to_world_transform = make_transform(rotation, state->debug_camera.to_world_transform.translation);
 }
 
 void bind_ui_font_material(any new_material_pointer, any old_material_pointer) {
@@ -100,14 +105,12 @@ APP_INIT_DEC(application_init) {
     
     state->debug_camera_alpha = 0.0f;
     state->debug_camera_beta = PIf * -0.25f;
-    state->camera_to_world_transform.translation = vec3f{ 0.0f, 10.0f, 10.0f };
-    update_camera(state);
-    
-    state->world_to_camera_transform = make_inverse_unscaled_transform(state->camera_to_world_transform);
+    state->debug_camera.to_world_transform.translation = vec3f{ 0.0f, 10.0f, 10.0f };
+    debug_update_camera(state);
     
     state->camera_to_clip_projection = make_perspective_fov_projection(60.0f, width_over_height(Reference_Resolution));
     
-    init_immediate_render_context(&state->imc, &state->world_to_camera_transform, &state->camera_to_clip_projection, KILO(64), &state->persistent_memory.allocator);
+    init_immediate_render_context(&state->immediate_render_context, &state->world_to_camera_transform, &state->camera_to_clip_projection, KILO(64), &state->persistent_memory.allocator);
     
     {
         init_ui_render_context(&state->ui_render_context, KILO(10), 512, KILO(4), &state->persistent_memory.allocator);
@@ -223,12 +226,16 @@ APP_INIT_DEC(application_init) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, 1, 1, 0, GL_RGB, GL_UNSIGNED_BYTE, &color);
     }
     
+    state->camera.to_world_transform = make_transform(make_quat(VEC3_X_AXIS, PIf * -0.5f), vec3f{ 0.0f, 80.0f, 0.0f });
+    state->main_window_area = { -1, -1, 800, 450 };
+    
     return state;
 }
 
 APP_MAIN_LOOP_DEC(application_main_loop) {
     Application_State *state = CAST_P(Application_State, app_data_ptr);
-    
+    auto imc = &state->immediate_render_context;
+    auto ui = &state->ui_render_context;
     // check if .dll was reloaded
     {
 #if 0
@@ -244,29 +251,51 @@ APP_MAIN_LOOP_DEC(application_main_loop) {
             init_gl();
     }
     
-    state->main_window_area = { (s16)CW_USEDEFAULT, (s16)CW_USEDEFAULT, 800, 450 };
+    // alt + F4 close application
+    if (input->left_alt.is_active && was_pressed(input->keys[VK_F4]))
+        PostQuitMessage(0);
     
-    if (!platform_api->window(platform_api, 0, S("Astroids"), &state->main_window_area))
+    // toggle fullscreen, this may freez the app for about 5 seconds
+    if (input->left_alt.is_active && was_pressed(input->keys[VK_RETURN]))
+        state->main_window_is_fullscreen = !state->main_window_is_fullscreen;
+    
+    
+    // enable to keep Reference_Resolution aspect ratio and add black borders
+#if 0
+    if (!platform_api->window(platform_api, 0, S("Astroids"), &state->main_window_area, true, state->main_window_is_fullscreen, 0.0f))
         PostQuitMessage(0);
     
     Pixel_Dimensions render_resolution = get_auto_render_resolution(state->main_window_area.size, Reference_Resolution);
+#else // forece window aspect ratio like Reference_Resolution
+    if (!platform_api->window(platform_api, 0, S("Astroids"), &state->main_window_area, true, state->main_window_is_fullscreen, width_over_height(Reference_Resolution)))
+        PostQuitMessage(0);
+    
+    Pixel_Dimensions render_resolution = state->main_window_area.size;
+#endif
     
     if (render_resolution.width == 0 || render_resolution.height == 0)
         return;
     
-    //clear(&state->transient_memory.memory_growing_stack);
+    clear(&state->transient_memory.memory_growing_stack);
     
-    set_auto_viewport(state->main_window_area.size, render_resolution, vec4f{ 0.05f, 0.05f, 0.05f, 1.0f }, vec4f{ 0.0f, 0.5f, 0.5f, 1.0f });
+    set_auto_viewport(state->main_window_area.size, render_resolution, vec4f{ 0.05f, 0.05f, 0.05f, 1.0f }, vec4f{ 0.05f, 0.05f, 0.05f, 1.0f });
     
-    UI_Render_Context *ui = &state->ui_render_context;
-    //ui_set_transform(ui, render_resolution, 1.0f);
-    ui_set_transform(ui, Reference_Resolution, 2.0f);
+    // enable to scale text relative to Reference_Resolution
+#if 0
+    ui_set_transform(ui, Reference_Resolution, 1.0f);
+#else
+    ui_set_transform(ui, render_resolution, 1.0f);
+#endif
+    
     ui_set_font(ui, &state->font, CAST_P(Render_Material, &state->ui_font_material));
     
     ui_printf(ui, 0, 0, S("some text"));
     
+    if (was_pressed(input->keys[VK_F1]))
+        state->in_debug_mode = !state->in_debug_mode;
+    
     // update debug camera
-    {
+    if (state->in_debug_mode) {
         if (was_pressed(input->mouse.right))
             state->last_mouse_window_position = input->mouse.window_position;
         else if (input->mouse.right.is_active) {
@@ -278,7 +307,7 @@ APP_MAIN_LOOP_DEC(application_main_loop) {
             state->debug_camera_beta -= mouse_delta.y * Camera_Mouse_Sensitivity;
             state->debug_camera_beta = CLAMP(state->debug_camera_beta, PIf * -0.5f, PIf * 0.5f);
             
-            update_camera(state);
+            debug_update_camera(state);
         }
         
         vec3f direction = {};
@@ -303,17 +332,30 @@ APP_MAIN_LOOP_DEC(application_main_loop) {
         
         direction = normalize_or_zero(direction);
         
-        state->camera_to_world_transform.translation +=  transform_direction(state->camera_to_world_transform, direction) * delta_seconds * Camera_Move_Speed;
+        state->debug_camera.to_world_transform.translation +=  transform_direction(state->debug_camera.to_world_transform, direction) * delta_seconds * Camera_Move_Speed;
+        state->world_to_camera_transform =  make_inverse_unscaled_transform(state->debug_camera.to_world_transform);
         
-        state->world_to_camera_transform =  make_inverse_unscaled_transform(state->camera_to_world_transform);
+        vec3f imc_view_direction = -state->debug_camera.to_world_transform.forward;
+        
+        // draw game camera
+        // note that the matrix.forward vector points to the opposite position of the camera view direction (the blue line)
+        draw_circle(imc, state->camera.to_world_transform.translation, imc_view_direction, 1.0f, make_rgba32(0.0f, 1.0f, 1.0f));
+        draw_line(imc, state->camera.to_world_transform.translation, state->camera.to_world_transform.translation + state->camera.to_world_transform.right, make_rgba32(1.0f, 0.0f, 0.0f));
+        draw_line(imc, state->camera.to_world_transform.translation, state->camera.to_world_transform.translation + state->camera.to_world_transform.up, make_rgba32(0.0f, 1.0f, 0.0f));
+        draw_line(imc, state->camera.to_world_transform.translation, state->camera.to_world_transform.translation + state->camera.to_world_transform.forward, make_rgba32(0.0f, 0.0f, 1.0f));
+        
+        // draw origin
+        draw_circle(imc, vec3f{}, VEC3_Y_AXIS, 1.0f, make_rgba32(1.0f, 0.0f, 1.0f));
+        draw_circle(imc, vec3f{}, imc_view_direction, 1.0f, make_rgba32(1.0f, 1.0f, 1.0f));
+        draw_line(imc, vec3f{}, VEC3_X_AXIS * 5, make_rgba32(1.0f, 0.0f, 0.0f));
+        draw_line(imc, vec3f{}, VEC3_Y_AXIS * 5, make_rgba32(0.0f, 1.0f, 0.0f));
+        draw_line(imc, vec3f{}, VEC3_Z_AXIS * 5, make_rgba32(0.0f, 0.0f, 1.0f));
     }
-    
-    auto imc = &state->imc;
-    
-    draw_circle(imc, vec3f{}, VEC3_Y_AXIS, 1.0f, make_rgba32(1.0f, 1.0f, 1.0f));
-    draw_line(imc, vec3f{}, VEC3_X_AXIS, make_rgba32(1.0f, 0.0f, 0.0f));
-    draw_line(imc, vec3f{}, VEC3_Y_AXIS, make_rgba32(0.0f, 1.0f, 0.0f));
-    draw_line(imc, vec3f{}, VEC3_Z_AXIS, make_rgba32(0.0f, 0.0f, 1.0f));
+    else {
+        state->world_to_camera_transform =  make_inverse_unscaled_transform(state->camera.to_world_transform);
+        
+        
+    }
     
     glUseProgram(state->phong_shader.program_object);
     glUniform3fv(state->phong_shader.u_light_world_position, 1, vec3f{ 0.0f, 2.0f, 0.0f });
@@ -327,7 +369,6 @@ APP_MAIN_LOOP_DEC(application_main_loop) {
         Render_Material *materials[] = { CAST_P(Render_Material, &state->astroid_material) };
         draw(&state->astroid_mesh.batch, ARRAY_WITH_COUNT(materials));
     }
-    
     
     {
         Render_Material *materials[] = { CAST_P(Render_Material, &state->beam_material) };
